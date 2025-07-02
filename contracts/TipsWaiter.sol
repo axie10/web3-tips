ead8327de52de434ceb6b7bb794b8efba8c3091a
axie10 <cryptosushi21@gmail.com>
2025-07-02 09:46:55 +0200

    Enhance Tips contract by adding balanceTotal variable and improving event definitions for clarity

--- contracts/TipsWaiter.sol
+++ contracts/TipsWaiter.sol
@@ -8,22 +8,41 @@ contract Tips {
 
     // Variable
     address public owner;
+    uint256 public balanceTotal;
     mapping(address => uint256) public tipsRecived;
 
-    // Events
-    event TipsRecived (address index, uint256 amount);
+    // Events (use standars "indexed" and "from/to" to transparency, traceability, compatibility)
+    event TipsRecived (address indexed from, uint256 amount);
+    event Withdrawal (address indexed to, uint256 amount);
 
     constructor () {
         owner = msg.sender;
     }
 
     function sendTips() public payable{
-        require(msg.value > 0, "eth has more than 0");
+        require(msg.value > 0, "ETH has more than 0");
 
         tipsRecived[msg.sender] += msg.value;
 
         emit TipsRecived(msg.sender, msg.value);
     }
 
+    function withdraw() public {
+
+        require(msg.sender == owner, "you are not a owner of this smart contract");
+        uint256 balance_ = address(this).balance;
+
+        require(balance_ > 0, "you haven`t enough ETH");
+
+        //* owner es una dirección (address)
+        //* payable(...) convierte una dirección normal en una dirección que puede recibir ETH
+        //* Esto envía balance wei (o ETH) a esa dirección
+        payable(owner).transfer(balance_);
+        emit Withdrawal(msg.sender, balance_);
+    }
+
+    function balanceCurrentContract() public view returns(uint256){
+        return address(this).balance;
+    }
 
 }
