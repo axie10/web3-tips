8e76fce5b46545d66440eb4a292892db7ac968d9
axie10 <cryptosushi21@gmail.com>
2025-07-02 09:01:08 +0200

    Refactor Tips contract to improve structure and readability

--- contracts/TipsWaiter.sol
+++ contracts/TipsWaiter.sol
@@ -6,10 +6,23 @@ pragma solidity ^0.8.24;
 
 contract Tips {
 
+    // Variable
     address public owner;
+    mapping(address => uint256) public tipsRecived;
+
+    // Events
+    event TipsRecived (address index, uint256 amount);
 
     constructor () {
         owner = msg.sender;
+    }
+
+    function sendTips() public payable{
+        require(msg.value > 0, "eth has more than 0");
+
+        tipsRecived[msg.sender] += msg.value;
+
+        emit TipsRecived(msg.sender, msg.value);
     }
 
 
